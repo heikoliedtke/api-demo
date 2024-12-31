c9f12953e66df71bb543d559913e9c81a7eb04e1
heikoliedtke <heiko.liedtke@dimensiondata.com>
2024-12-31 14:31:15 +0100

    Initial Commit

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+@app.route('/hello')
+def hello():
+    """
+    Returns a JSON response with the text "Hello World"
+    """
+    return jsonify({'text': 'Hello World'})
+
+@app.route('/wassup')
+def wassup():
+    """
+    Returns a JSON response with the text "Wassup"
+    """
+    return jsonify({'text': 'Wassup'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
